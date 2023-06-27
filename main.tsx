cee95bec15ebdcb37fa4f71debcc3a7e61b2aad2
Eve-nichakon <91275476+nichakontae@users.noreply.github.com>
2023-06-27 22:18:46 +0700

    install react router dom

--- main.tsx
+++ main.tsx
@@ -4,6 +4,7 @@ import App from "./src/App.js";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  // <React.StrictMode> use for dev mode when production should not use this
   <React.StrictMode>
     <App />
   </React.StrictMode>
