dda67af34ba3ff252b15b207f07b9da249526b8d
Eve-nichakon <91275476+nichakontae@users.noreply.github.com>
2023-06-27 22:38:24 +0700

    add color theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import { ThemeProvider } from '@chakra-ui/react'
-import React from 'react'
-import AppRouter from './router'
+import { ChakraProvider } from "@chakra-ui/react";
+import React from "react";
+import AppRouter from "./router";
+import theme from "./theme/theme";
 
 const App = () => {
   return (
     // <React.Fragment> used for grouping multiple elements without adding extra DOM nodes be like <>...</>
     <React.Fragment>
-
-         <AppRouter/>
-
+      <ChakraProvider theme={theme}>
+        <AppRouter />
+      </ChakraProvider>
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default App
+export default App;
