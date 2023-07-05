77f49907deb6cb575c0b9a223cac2b383ef5fc27
Eve-nichakon <91275476+nichakontae@users.noreply.github.com>
2023-07-06 02:50:30 +0700

    Update theme.tsx

---
+++ src/theme/theme.tsx
@@ -0,0 +1,42 @@
+export const lightMode = {
+    bg: {
+      main: "#FFFFFF",
+      200: "#f5f5f5",
+      300: "#cccccc",
+    },
+    primary: {
+      main: "#4D648D",
+      200: "#7C91BD",
+      300: "#DFF4FF",
+    },
+    accent: {
+      main: "#A7B6D7",
+      200: "#4A5A77",
+    },
+    text: {
+      main: "#000000",
+      200: "#2C2C2C",
+    },
+  };
+  
+  export const darkMode = {
+    bg: {
+      main: "#111A29",
+      200: "#1E293A",
+      300: "#364053",
+    },
+    primary: {
+      main: "#4D648D",
+      200: "#7C91BD",
+      300: "#DFF4FF",
+    },
+    accent: {
+      main: "#A7B6D7",
+      200: "#4A5A77",
+    },
+    text: {
+      main: "#ffffff",
+      200: "#E0E0E0",
+    },
+  };
+  
