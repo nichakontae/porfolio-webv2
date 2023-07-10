4b0fe978741fe6f064af54ce147bbe9df2554c37
Eve-nichakon <91275476+nichakontae@users.noreply.github.com>
2023-07-10 21:02:43 +0700

    update theme

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,9 @@ import ThemeProvider from "@mui/material/styles/ThemeProvider";
 import { PaletteMode, createTheme, useMediaQuery } from "@mui/material";
 
 const App = () => {
-  const [mode, setMode] = useState<PaletteMode>("light");
   const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
-  useEffect(() => {
-    setMode(prefersDarkMode ? "dark" : "light");
-  }, [mode]);
-
-  const theme = useMemo(
-    () => createTheme(getDesignTokens(mode)),
-    [prefersDarkMode]
-  );
+  const mode = prefersDarkMode ? "dark" : "light";
+  const theme = useMemo(() => createTheme(getDesignTokens(mode)), [mode]);
 
   return (
     // <React.Fragment> used for grouping multiple elements without adding extra DOM nodes be like <>...</>
