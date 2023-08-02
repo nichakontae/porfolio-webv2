4295f30fa26705635a5785f5222facea38080c38
Eve-nichakon <91275476+nichakontae@users.noreply.github.com>
2023-08-02 21:48:11 +0700

    move assets to public folder

--- src/resources/experience.ts
+++ src/resources/experience.ts
@@ -4,6 +4,6 @@ export const workExperience: workExperienceType[] = [
     position: "Backend developer",
     desc: "During my time at Connected Tech, I worked as a freelance developer, contributing to the development of various APIs for the Denso's system. This role provided me with invaluable hands-on experience in software development and exposed me to the real-world challenges of working on a professional project. I collaborated with a team of developers, gaining valuable insights into teamwork and communication within a professional setting. Through this experience, I honed my coding skills and deepened my understanding of software architecture. It was a transformative opportunity that allowed me to grow both personally and professionally.",
     date: "July 2023",
-    img: "src/assets/images/proj/denso.png",
+    img: "./assets/images/proj/denso.png",
   },
 ];
--- src/resources/intro_each_page.ts
+++ src/resources/intro_each_page.ts
@@ -3,7 +3,7 @@ import { IntroEachPageType } from "../types/intro";
 export const IntroEachPage: IntroEachPageType[] = [
   {
     path: "/home",
-    image: "src/assets/images/profile.jpg",
+    image: "./assets/images/profile.jpg",
     smallTopic: "Hi, I’m Eve 👋",
     topic: "Full-stack software developer",
     topic2: "Photographer",
@@ -32,19 +32,19 @@ export const IntroEachPage: IntroEachPageType[] = [
   },
   {
     path: "/projects",
-    image: "src/assets/images/profile.jpg",
+    image: "./assets/images/profile.jpg",
     topic: "Projects",
     desc: "Welcome to my project showcase! In this section, I present my latest web development projects and share my passion for creating innovative and user-friendly websites. Each project represents my skills and expertise in various technologies and showcases my problem-solving abilities. Explore the projects, get inspired, and discover the possibilities of web development. Let's build something amazing together!",
   },
   {
     path: "/photographs",
-    image: "src/assets/images/profile.jpg",
+    image: "./assets/images/profile.jpg",
     topic: "Photographs",
     desc: "Step into the world of photography with me! Here, I unleash my creativity and capture captivating moments through the lens. From stunning landscapes to vibrant portraits, I strive to convey emotions and tell stories through my photographs. Join me on this photography tour to see the world's beauty via my lens. Feel free to browse through the galleries and let the pictures speak to your heart.",
   },
   {
     path: "/about",
-    image: "src/assets/images/profile.jpg",
+    image: "./assets/images/profile.jpg",
     topic: "About",
     desc: "Welcome to my personal space! I'm excited to tell you more about myself and my story. I'm a 20-year-old student studying computer science with an intense passion for web programming. In addition to my passion of coding, I also find comfort in photography since it allows me to capture lovely moments in time. I want to use this website to share my work, my experiences, and to meet other like-minded people. Let's work together, share ideas, and take on fascinating initiatives. Join me on this adventure as I continue to learn, grow, and explore the vast possibilities of the digital world.",
   },
